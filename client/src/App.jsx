2e6979f959472e41d5948d624e5d6212c0550451
Eregion <wuxize1995@gmail.com>
2023-07-30 16:04:04 -0400

    review -refactor axios request & remove comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,37 +6,17 @@ import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
-  const test = ()=>{
-    axios.post('http://localhost:8080/test',{earl_grey: false})
-    .then(response =>{console.log(response)})
-    .catch(function (error) {
-      console.log(error)
-    });  
+  const test = async () => {
+    try {
+      const response = await axios.post('http://localhost:8080/test', { earl_grey: false })
+      console.log(response)
+    }
+    catch (err) {
+      console.log(err)
+    }
   }
   return (
-  <button onClick={()=>test()}>Test</button>
-remove all the comments and state
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
+    <button onClick={() => test()}>Test</button>
   )
 }
 
