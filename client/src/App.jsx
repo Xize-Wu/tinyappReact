17dfe85442bb2e5d4d61892ce2412fffb71b4f95
Aster Wu <61925262+Xize-Wu@users.noreply.github.com>
2023-07-30 15:57:01 -0400

    Update client/src/App.jsx
    Co-authored-by: Vasiliy Klimkin <vasily.klimkin@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   }
   return (
   <button onClick={()=>test()}>Test</button>
-    // <>
+remove all the comments and state
     //   <div>
     //     <a href="https://vitejs.dev" target="_blank">
     //       <img src={viteLogo} className="logo" alt="Vite logo" />
