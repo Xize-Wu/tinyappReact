5664f9f0c337009907684ae878d3cdd0cb65043b
Eregion <wuxize1995@gmail.com>
2023-08-05 00:17:46 -0400

    refactor profile page with react route

---
+++ client/src/routes/profile.jsx
@@ -0,0 +1,10 @@
+import { Form } from "react-router-dom";
+
+export default function Profile (){
+    return (
+        <div id='profile'>
+
+            This is the profile page
+        </div>
+    )
+}
